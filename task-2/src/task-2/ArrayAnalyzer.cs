b4efb1cb120146172b3a3a6611bb45c411110dc9
XmyriyCat <aleskevic79@gmail.com>
2023-07-13 12:58:53 +0300

    Completed task-2

---
+++ task-2/src/task-2/ArrayAnalyzer.cs
@@ -0,0 +1,24 @@
+﻿namespace task_2;
+
+public class ArrayAnalyzer
+{
+    public static void GetUsageOfEachNumber(ICollection<int> numbers)
+    {
+        var numberUsageDictionary = new Dictionary<int, int>(numbers.Count);
+
+        foreach (var value in numbers)
+        {
+            if (numberUsageDictionary.ContainsKey(value))
+            {
+                numberUsageDictionary[value]++;
+                continue;
+            }
+            
+            numberUsageDictionary.Add(value, 1);
+        }
+
+        var usageStat = string.Join("\n", numberUsageDictionary.Select(x => $"the number: {x.Key} -> is used {x.Value} times"));
+            
+        Console.WriteLine(usageStat);
+    }
+}
---
+++ task-2/src/task-2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace task_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new[] { 1, 2, 5, 3, 2, 4, 5, 6, 2, 1, 4, 6, 6, 6, 7, 1, 3, 4 };
+
+            ArrayAnalyzer.GetUsageOfEachNumber(numbers);
+        }
+    }
+}
